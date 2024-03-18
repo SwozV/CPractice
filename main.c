1e2af33f50ac9be0f7e9210a127020b97dcd6b36
Kyon <swoz.08.v@outlook.com>
2024-03-18 14:24:36 +0800

    GPT oriented programming...

--- main.c
+++ main.c
@@ -1,8 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+#define BUFFER_SIZE 1024
+
+void analyzePacket(char* buffer, int size) {
+    printf("Received packet: \n");
+    for (int i = 0; i < size; i++) {
+        printf("%02X ", (unsigned char)buffer[i]);
+        if ((i + 1) % 16 == 0)
+            printf("\n");
+    }
+    printf("\n");
+}
+
 int main() {
-    char name[10];
-    printf("Input your name: ");
-    // scanf("%s", name);
-    printf("Hello,%s,this is your vscode!\n", name);
+    WSADATA wsaData;
+    SOCKET sockfd;
+    struct sockaddr_in serverAddr;
+    int addrLen = sizeof(struct sockaddr_in);
+    char buffer[BUFFER_SIZE];
+    int bytesReceived;
+
+    // 初始化Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed.\n");
+        return 1;
+    }
+
+    // 创建套接字
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET) {
+        printf("Failed to create socket.\n");
+        return 1;
+    }
+
+    // 设置服务器地址
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(8888);
+
+    // 绑定套接字
+    if (bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        printf("Bind failed.\n");
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Listening for incoming packets...\n");
+
+    while (1) {
+        bytesReceived = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&serverAddr, &addrLen);
+        if (bytesReceived == SOCKET_ERROR) {
+            printf("recvfrom() failed.\n");
+            break;
+        }
+
+        // 处理接收到的数据包
+        analyzePacket(buffer, bytesReceived);
+    }
+
+    // 关闭套接字和Winsock
+    closesocket(sockfd);
+    WSACleanup();
+
     return 0;
 }
